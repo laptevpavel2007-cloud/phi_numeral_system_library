906b6b1b664c124e2d05bd7fa2a737339aad34a0
root <root@LAPTOP-3I9FKNT7.localdomain>
2026-07-02 11:03:39 +0800

    Revert "Removed arythmetics from PhiNumber and test_PhiNumber"
    This reverts commit bb9c4dbd703745dd236ac271b627ff8400955247.

--- test_PhiNumber.py
+++ test_PhiNumber.py
@@ -8,6 +8,11 @@ def test_creation():
     assert (x.a, x.b) == (Fraction(5), Fraction(0))
     assert P.phi() == P(Fraction(1, 2), Fraction(1, 2))
  
+def test_arithmetic():
+    assert P(0, 1) * P(0, 1) == P(5, 0)    
+    assert P.phi() * P.phi() == P.phi() + P(1, 0) 
+    assert P(1, 2) + P(3, 4) == P(4, 6) 
+    assert P(1, 2) - P(3, 4) == P(-2, -2) 
  
 def test_exactness():
     third = P(Fraction(1, 3), 0)
